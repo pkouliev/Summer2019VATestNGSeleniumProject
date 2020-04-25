c6ab153772084edff38edd8bb916a97b1b598139
pkouliev <pkouliev@gmail.com>
2020-04-24 23:11:22 -0400

    day 1 basic navigation

---
+++ src/test/java/com/cybertek/tests/d1_basic_navigation/OpenBrowserDemo.java
@@ -0,0 +1,24 @@
+package com.cybertek.tests.d1_basic_navigation;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class OpenBrowserDemo {
+
+    public static void main(String[] args) {
+
+        // option+enter MAC
+        // alt+enter WIN
+
+        // we HAVE TO enter this line every time we want to open
+        // without it we get exception
+        WebDriverManager.chromedriver().setup();
+
+        // this part is selenium
+        // WebDriver --> represents browser
+        // new ChromeDriver() --> this line opens chrome browser
+        WebDriver driver = new ChromeDriver();
+    }
+}
