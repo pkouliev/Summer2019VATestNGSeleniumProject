250d982d6d25fcd6eb431dc195cdd46bb3f256db
pkouliev <pkouliev@gmail.com>
2020-04-25 23:00:28 -0400

    WebDriverFactory

---
+++ src/test/java/com/cybertek/utilities/WebDriverFactory.java
@@ -0,0 +1,48 @@
+package com.cybertek.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverFactory {
+
+    // write a static method that takes a string parameter
+    // based on the value of the parameter
+    // the method must return chromedriver or firefoxdriver object
+    // name: getDriver
+
+//    public static WebDriver getDriver(String browserType) {
+//
+//        if (browserType.toLowerCase().equalsIgnoreCase("Chrome")) {
+//            WebDriverManager.chromedriver().setup();
+//            // WebDriver driver = new ChromeDriver();
+//            // return driver;
+//            return new ChromeDriver();
+//        } else if (browserType.toLowerCase().equalsIgnoreCase("Firefox")) {
+//            WebDriverManager.firefoxdriver().setup();
+//            // WebDriver driver = new FirefoxDriver();
+//            // return driver;
+//            return new FirefoxDriver();
+//        } else {
+//            return null;
+//        }
+//    }
+
+    public static WebDriver getDriver(String browserType) {
+        WebDriver driver = null;
+        switch (browserType.toLowerCase()) {
+            case "chrome" -> {
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+            }
+            case "firefox" -> {
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+            }
+        }
+
+        return driver;
+
+    }
+}
