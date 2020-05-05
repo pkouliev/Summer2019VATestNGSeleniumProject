3c1e7538ff96dd5bff3adee092ec12b8a899e21c
pkouliev <pkouliev@gmail.com>
2020-05-05 19:14:32 -0400

    NameLocatorTest.java

---
+++ src/test/java/com/cybertek/tests/d4_basic_locators/NameLocatorTest.java
@@ -0,0 +1,27 @@
+package com.cybertek.tests.d4_basic_locators;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class NameLocatorTest {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("http://practice.cybertekschool.com/sign_up");
+
+        WebElement fullName = driver.findElement(By.name("full_name"));
+        fullName.sendKeys("John Doe");
+
+        WebElement email = driver.findElement(By.name("email"));
+        email.sendKeys("johndoe@cybertekschool.com");
+
+        WebElement submitButton = driver.findElement(By.name("wooden_spoon"));
+        submitButton.click();
+
+        Thread.sleep(3000);
+        driver.quit();
+
+    }
+}
